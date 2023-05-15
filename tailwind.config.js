408d4386e5ce2e79248cb5fae00c86038926ea41
shylabo <shuya.sohot@gmail.com>
2023-05-14 18:02:05 -0700

    update: css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/styles/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        mont: ['var(--font-mont)', ...fontFamily.sans],
       },
+      colors: {
+        dark: '#1b1b1b',
+        light: '#f5f5f5',
+        primary: '#0096FF',
+        primaryDark: '#00FFCA',
+      },
+      animation: {
+        'spin-slow': 'spin 8s linear infinite',
+      },
+      backgroundImage: {},
+    },
+    screens: {
+      '2xl': { max: '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: '639px' },
+      // => @media (max-width: 639px) { ... }
+
+      xs: { max: '479px' },
+      // => @media (max-width: 479px) { ... }
     },
   },
   plugins: [],
