6d10e43273e09259ff1d697b591952413287d8ff
shylabo <shuya.sohot@gmail.com>
2023-05-14 18:01:53 -0700

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+import Layout from './Layout'
+
+const Footer = () => {
+  return (
+    <footer className="w-full font-medium border-t-2 border-solid border-dark text-lg dark:text-light dark:border-light | sm:text-base">
+      <Layout className="flex items-center justify-between lg:flex-col lg:py-6">
+        <div className="text-primary">Shuya Life</div>
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+        <Link href="https://medium.com/@shuya.tech" target="_blank" className="underline underline-offset-2">
+          Blue pill or Red pill?
+        </Link>
+      </Layout>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import '@/styles/globals.css'
 import Head from 'next/head'
 import { AppProps } from 'next/app'
@@ -10,8 +11,9 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main className=" w-full min-h-screen">
+      <main className=" w-full min-h-screen bg-light dark:bg-dark">
         <Component key={router.asPath} {...pageProps} />
+        <Footer />
       </main>
     </>
   )
