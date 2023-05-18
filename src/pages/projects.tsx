64e7e15fdc64b2ba741259a3e5ff098a9734dcfd
shylabo <shuya.sohot@gmail.com>
2023-05-17 20:06:03 -0700

    feat: add projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,204 @@
+import AnimatedText from '@/components/AnimatedText'
+import { GithubIcon } from '@/components/Icons'
+import Layout from '@/components/Layout'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import dalle from '../../public/images/projects/dalle.png'
+import syncfusionDashboard from '../../public/images/projects/syncfusion-dashboard.png'
+import aiChatRoom from '../../public/images/projects/ai-chat-room.png'
+import { motion } from 'framer-motion'
+
+const FramerImage = motion(Image)
+
+const FeaturedProject = ({
+  type,
+  title,
+  summary,
+  img,
+  link,
+  github,
+}: {
+  type: string
+  title: string
+  summary: string
+  img: any
+  link: string
+  github: string
+}) => {
+  return (
+    <article
+      className="w-full flex items-center justify-between relative  rounded-br-2xl
+        rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light
+        lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4"
+    >
+      <div
+        className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light
+    rounded-br-3xl xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]
+    "
+      />
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
+      >
+        <FramerImage
+          src={img}
+          alt={title}
+          className="w-full h-auto"
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+          priority
+          sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              50vw"
+        />
+      </Link>
+
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
+        <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">
+          {type}
+        </span>
+        <Link href={link} target="_blank" className="hover:underline underline-offset-2">
+          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light lg:text-3xl xs:text-2xl">
+            {title}
+          </h2>
+        </Link>
+        <p className="my-2 font-medium text-dark dark:text-light sm:text-sm">{summary}</p>
+        <div className="mt-2 flex items-center">
+          <Link href={github} target="_blank" className="w-10">
+            {' '}
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold
+            dark:bg-light dark:text-dark
+            sm:px-4 sm:text-base
+            "
+          >
+            Visit Project
+          </Link>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+const Project = ({
+  title,
+  type,
+  img,
+  link,
+  github,
+}: {
+  title: string
+  type: string
+  img: any
+  link: string
+  github: string
+}) => {
+  return (
+    <article
+      className="w-full flex flex-col items-center justify-center rounded-2xl 
+    border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4
+    "
+    >
+      <div
+        className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark
+    rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]
+    "
+      />
+      <Link
+        href={link}
+        target="_blank"
+        className="w-full cursor-pointer overflow-hidden rounded-lg"
+      >
+        <FramerImage
+          src={img}
+          alt={title}
+          className="w-full h-auto"
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+        />
+      </Link>
+
+      <div className="w-full flex flex-col items-start justify-between mt-4">
+        <span className="text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base">
+          {type}
+        </span>
+        <Link href={link} target="_blank" className="hover:underline underline-offset-2">
+          <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">{title}</h2>
+        </Link>
+
+        <div className="w-full flex items-center justify-between">
+          <Link
+            href={link}
+            target="_blank"
+            className="text-lg font-semibold underline md:text-base"
+          >
+            Visit
+          </Link>
+          <Link href={github} target="_blank" className="w-8 md:w-6">
+            <GithubIcon />
+          </Link>
+        </div>
+      </div>
+    </article>
+  )
+}
 
 const projects = () => {
-  return <div>projects</div>
+  return (
+    <>
+      <Head>
+        <title>Projects - ShuyaLife</title>
+        <meta name="description" content="projects" />
+      </Head>
+      <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Coding is always fun!"
+            className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
+          />
+
+          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
+            <div className="col-span-12">
+              <FeaturedProject
+                type="Featured Project"
+                title="Image Generation App Powered by OpenAI API"
+                summary="This is an application that allows you to generate an image based on your script using the OpenAI API. With this application, you can easily create custom images that accurately represent your ideas and concepts. The process is simple. You input your script into the application, and the OpenAI API generates an image based on your description. You can then preview and download the image, making it easy to incorporate into your projects or share with others.
+                "
+                img={dalle}
+                link="/404"
+                github="https://github.com/shylabo/dalle"
+              />
+            </div>
+            <div className="col-span-6 sm:col-span-12">
+              <Project
+                type="Dashboard App"
+                title="Syncfusion dashboard"
+                img={syncfusionDashboard}
+                link="/404"
+                github="https://github.com/shylabo/syncfusion_dashboard"
+              />
+            </div>
+            <div className="col-span-6 sm:col-span-12">
+              <Project
+                type="Chat APP"
+                title="AI Chat Room Powered by OpenAI API"
+                img={aiChatRoom}
+                link="/404"
+                github="https://github.com/shylabo/ai-chat-room"
+              />
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  )
 }
 
 export default projects
