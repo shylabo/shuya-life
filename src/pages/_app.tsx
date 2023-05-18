653cc3364b81c6fa896b4c710f171d925d18f20f
shylabo <shuya.sohot@gmail.com>
2023-05-17 20:01:37 -0700

    update _app & _document files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,40 @@
-import '@/styles/globals.css'
-import NavBar from '@/components/NavBar'
 import Footer from '@/components/Footer'
+import NavBar from '@/components/NavBar'
+import '@/styles/globals.css'
+import { AnimatePresence } from 'framer-motion'
+import { Montserrat } from 'next/font/google'
 import Head from 'next/head'
-import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
+import { PropsWithChildren } from 'react'
 
-function App({ Component, pageProps }: AppProps) {
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-mont',
+})
+
+export default function App({
+  Component,
+  pageProps,
+}: {
+  Component: any
+  pageProps: PropsWithChildren
+}) {
   const router = useRouter()
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=" w-full min-h-screen bg-light dark:bg-dark">
+      <main
+        className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
+      >
         <NavBar />
-        <Component key={router.asPath} {...pageProps} />
+        <AnimatePresence initial={false} mode="wait">
+          <Component key={router.asPath} {...pageProps} />
+        </AnimatePresence>
         <Footer />
       </main>
     </>
   )
 }
-
-export default App
