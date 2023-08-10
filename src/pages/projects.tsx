49fc854bef955980e9aeb5e8f269210e5dbb8b68
shylabo <shuya.sohot@gmail.com>
2023-08-09 18:57:21 -0700

    update: title of projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -39,11 +39,7 @@ const FeaturedProject = ({
     rounded-br-3xl xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]
     "
       />
-      <Link
-        href={link}
-        target="_blank"
-        className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
-      >
+      <Link href={link} target="_blank" className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full">
         <FramerImage
           src={img}
           alt={title}
@@ -58,13 +54,9 @@ const FeaturedProject = ({
       </Link>
 
       <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">
-          {type}
-        </span>
+        <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">{type}</span>
         <Link href={link} target="_blank" className="hover:underline underline-offset-2">
-          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light lg:text-3xl xs:text-2xl">
-            {title}
-          </h2>
+          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light lg:text-3xl xs:text-2xl">{title}</h2>
         </Link>
         <p className="my-2 font-medium text-dark dark:text-light sm:text-sm">{summary}</p>
         <div className="mt-2 flex items-center">
@@ -112,11 +104,7 @@ const Project = ({
     rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]
     "
       />
-      <Link
-        href={link}
-        target="_blank"
-        className="w-full cursor-pointer overflow-hidden rounded-lg"
-      >
+      <Link href={link} target="_blank" className="w-full cursor-pointer overflow-hidden rounded-lg">
         <FramerImage
           src={img}
           alt={title}
@@ -127,19 +115,13 @@ const Project = ({
       </Link>
 
       <div className="w-full flex flex-col items-start justify-between mt-4">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base">
-          {type}
-        </span>
+        <span className="text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base">{type}</span>
         <Link href={link} target="_blank" className="hover:underline underline-offset-2">
           <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">{title}</h2>
         </Link>
 
         <div className="w-full flex items-center justify-between">
-          <Link
-            href={link}
-            target="_blank"
-            className="text-lg font-semibold underline md:text-base"
-          >
+          <Link href={link} target="_blank" className="text-lg font-semibold underline md:text-base">
             Visit
           </Link>
           <Link href={github} target="_blank" className="w-8 md:w-6">
@@ -160,17 +142,14 @@ const projects = () => {
       </Head>
       <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
-          <AnimatedText
-            text="Coding is always fun!"
-            className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
-          />
+          <AnimatedText text="Coding is always fun!" className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl" />
 
           <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
             <div className="col-span-12">
               <FeaturedProject
                 type="Featured Project"
-                title="Full Stack Travel Booking App"
-                summary="This travel booking app offers an extensive range of destinations worldwide, allowing you to book numerous places across the globe. It provides convenient features such as third-party login integration, efficient search functionality, seamless reservation processes, and comprehensive property management capabilities. Additionally, the app boasts a modern and fully responsive design, making it visually appealing and user-friendly for an optimal booking experience. Developed as a full stack application using Next.js and React, it combines robust functionality with cutting-edge technology to deliver a top-notch travel booking platform.
+                title="Travel Booking App"
+                summary="This travel booking app offers an extensive range of destinations worldwide, allowing you to book numerous places across the globe. It provides convenient features such as third-party login integration, efficient search functionality, seamless reservation processes, and comprehensive property management capabilities. Additionally, the app boasts a modern and fully responsive design, making it visually appealing and user-friendly for an optimal booking experience. Developed as an application using Next.js and React, it combines robust functionality with cutting-edge technology to deliver a top-notch travel booking platform.
                 "
                 img={travelAnywhere}
                 link="https://travel-anywhere.vercel.app/"
@@ -188,7 +167,7 @@ const projects = () => {
             </div>
             <div className="col-span-6 sm:col-span-12">
               <Project
-                type="Full stack project"
+                type="ChatGPT"
                 title="Image Generation App Powered by OpenAI API"
                 img={dalle}
                 link="https://dalle-web-65vw.onrender.com/"
@@ -197,7 +176,7 @@ const projects = () => {
             </div>
             <div className="col-span-6 sm:col-span-12">
               <Project
-                type="Chat APP"
+                type="ChatGPT"
                 title="AI Chat Room Powered by OpenAI API"
                 img={aiChatRoom}
                 link="https://ai-chat-room-client.onrender.com/"
